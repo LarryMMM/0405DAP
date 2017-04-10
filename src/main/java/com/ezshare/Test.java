1cb9f6dbf6b8ef5f883e0f4f2e569981300e2e41
jason <zhaizenan@hotmail.com>
2017-04-10 10:11:50 +1000

    test commit of jason

--- src/main/java/com/ezshare/Test.java
+++ src/main/java/com/ezshare/Test.java
@@ -2,6 +2,8 @@ package com.ezshare;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Hello world! -From Jason");
     }
 }
