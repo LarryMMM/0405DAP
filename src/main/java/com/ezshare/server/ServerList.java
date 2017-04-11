44cd095fc57af7ee2385e162dddc9caaba4369b9
Wenhao Zhao <myprivatedisk@gmail.com>
2017-04-11 15:52:14 +1000

    ServerList bug fixed

--- src/main/java/com/ezshare/server/ServerList.java
+++ src/main/java/com/ezshare/server/ServerList.java
@@ -1,6 +1,7 @@
 package com.ezshare.server;
 
 import com.ezshare.message.Host;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,18 +9,21 @@ import java.util.List;
  * @author Wenhao Zhao
  */
 public class ServerList {
-    private List<Host> serverList;
+    private List<Host> serverList = new ArrayList<Host>();
     
     public synchronized List<Host> getServerList() {
         return serverList;
     }
             
-    public synchronized void updateServerList(List<Host> inputServerList) {
+    public synchronized int updateServerList(List<Host> inputServerList) {
+        int addCount = 0;
         for (Host inputHost : inputServerList) {
             if (!containsHost(inputHost)) {
                 serverList.add(inputHost);
+                ++addCount;
             }
         }
+        return addCount;
     }
     
     private synchronized boolean containsHost(Host inputHost) {
