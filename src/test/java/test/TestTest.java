dd2c9189b56afc7201110384d7dc727c13c2befa
Wenhao Zhao <myprivatedisk@gmail.com>
2017-04-18 14:48:16 +1000

    Added JUnit support

---
+++ src/test/java/test/TestTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author nek
+ */
+public class TestTest {
+    @Test
+    public void test() {
+        int sum = 1;
+        Assert.assertEquals(sum, 1);
+    }
+}
